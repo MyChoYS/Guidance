cc6534a81f8df38c78e005c6fb1cc5dd09c3e275
he-reme <rlagpfla1017@naver.com>
2021-02-04 14:37:46 +0900

    뼈대 수정

--- ChaChaCha/ChaProject/basicApp/urls.py
+++ ChaChaCha/ChaProject/basicApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('index/', views.index, name='index'),
+    path('home/', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
 ]
--- ChaChaCha/ChaProject/basicApp/views.py
+++ ChaChaCha/ChaProject/basicApp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def index(request):
-    return render(request, 'index.html', None)
+def home(request):
+    return render(request, 'home.html', None)
 
 def contact(request) :
     return render(request, 'contact.html', None)
