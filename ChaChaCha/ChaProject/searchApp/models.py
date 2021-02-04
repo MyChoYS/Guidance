f2a4e25426c10040a8911fed60006613269db102
he-reme <rlagpfla1017@naver.com>
2021-02-04 18:46:36 +0900

    academy 테이블 수정

--- ChaChaCha/ChaProject/searchApp/models.py
+++ ChaChaCha/ChaProject/searchApp/models.py
@@ -5,11 +5,11 @@ class Academy(models.Model) :
     address_full = models.CharField(max_length=80)
     address_city = models.CharField(max_length=45)
     address_town = models.CharField(max_length=45)
-    fee1 = models.IntegerField()
-    fee2 = models.IntegerField()
-    bus = models.TextField()
-    url = models.TextField()
-    phone_number = models.CharField(max_length=45)
+    fee1 = models.IntegerField(null=True)
+    fee2 = models.IntegerField(null=True)
+    bus = models.CharField(max_length=500, null=True)
+    phone_number = models.CharField(max_length=45, null=True)
+    url = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return "main {}-{}-{}-{}-{}-{}-{}-{}-{}-{}".format(self.id, self.name, self.address_full, self.address_city,
@@ -19,9 +19,9 @@ class Academy(models.Model) :
 class TestSite(models.Model) :
     name = models.CharField(max_length=45)
     address_full = models.CharField(max_length=45)
-    courseA = models.CharField(max_length=80)
-    courseB = models.CharField(max_length=80)
-    courseC = models.CharField(max_length=80)
-    courseD = models.CharField(max_length=80)
-    url = models.TextField()
+    courseA = models.CharField(max_length=500, null=True)
+    courseB = models.CharField(max_length=500, null=True)
+    courseC = models.CharField(max_length=500, null=True)
+    courseD = models.CharField(max_length=500, null=True)
     phone_number = models.CharField(max_length=45)
+    url = models.CharField(max_length=500, null=True)
