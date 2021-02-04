3b852b057fe75214f93ccc4e6c7affea447540aa
he-reme <rlagpfla1017@naver.com>
2021-02-04 15:47:43 +0900

    학원, 시험장 모델 클래스 추가

--- ChaChaCha/ChaProject/searchApp/models.py
+++ ChaChaCha/ChaProject/searchApp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Academy(models.Model) :
+    name = models.CharField(max_length=45)
+    address_full = models.CharField(max_length=80)
+    address_city = models.CharField(max_length=45)
+    address_town = models.CharField(max_length=45)
+    fee1 = models.IntegerField()
+    fee2 = models.IntegerField()
+    bus = models.TextField()
+    url = models.TextField()
+    phone_number = models.CharField(max_length=45)
+
+    def __str__(self):
+        return "main {}-{}-{}-{}-{}-{}-{}-{}-{}-{}".format(self.id, self.name, self.address_full, self.address_city,
+                                                           self.address_town, self.fee1, self.fee2, self.bus, self.url,
+                                                           self.phone_number)
+
+class TestSite(models.Model) :
+    name = models.CharField(max_length=45)
+    address_full = models.CharField(max_length=45)
+    courseA = models.CharField(max_length=80)
+    courseB = models.CharField(max_length=80)
+    courseC = models.CharField(max_length=80)
+    courseD = models.CharField(max_length=80)
+    url = models.TextField()
+    phone_number = models.CharField(max_length=45)
