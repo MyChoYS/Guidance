f7bbcfcc2d003599cea42950e6654819ba076d2a
Cho - Yong Sang <76713912+MyChoYS@users.noreply.github.com>
2021-02-05 10:44:19 +0900

    사고쳤읍니다
    죄송합미다.

--- ChaChaCha/ChaProject/bbsApp/models.py
+++ ChaChaCha/ChaProject/bbsApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from searchApp.models import Academy
+
+
+class Academy_review(models.Model):
+    password = models.CharField(max_length=8)
+    content = models.CharField(max_length=500)
+    academy_id = models.ForeignKey(Academy, on_delete=models.CASCADE)
+
+
+
+
+
+
 
 # Create your models here.
