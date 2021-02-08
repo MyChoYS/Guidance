bba03294dd9d2a952f7feeabbeff54f0805e1b31
Sanghyun An <ideaan789@gmail.com>
2021-02-08 09:17:10 +0900

    미완성본

--- ChaChaCha/ChaProject/searchApp/views.py
+++ ChaChaCha/ChaProject/searchApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Academy, TestSite
 
 # 학원 검색
 def services1(request) :
@@ -12,8 +13,14 @@ def services2(request) :
 
 # 내 주변 시험장 검색 (맵)
 def services3(request) :
-    return render(request, 'services3.html', None)
+    mylocation = Academy.objects.all()
+    context = {
+        'Academy':mylocation,
+    }
+    return render(request, 'services3.html', context)
 
+    ## thisUserMoments = mymoment.objects.filter(user=randomuserid, score__gte=4).order_by('-momentcreatedate', '-score')[0:4]
 # 지도 테스트 나중에 지우기
 def tt(request):
     return render(request, 'tt.html', None)
+
