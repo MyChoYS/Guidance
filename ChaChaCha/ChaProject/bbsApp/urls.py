6155122822e75ef98d4490c965496f6afd62506c
he-reme <rlagpfla1017@naver.com>
2021-02-08 11:22:53 +0900

    후기게시판 업로드

--- ChaChaCha/ChaProject/bbsApp/urls.py
+++ ChaChaCha/ChaProject/bbsApp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('review/', views.review, name='review'),
+    path('review/', views.review_academy, name='review'),
+    path('review_academy/', views.review_academy, name='review_academy'),
+    path('review_testsite/', views.review_testsite, name='review_testsite'),
+    path('delete_academy/', views.delete_academy, name='delete_academy'),
+    path('delete_testsite/', views.delete_testsite, name='delete_testsite'),
 ]
--- ChaChaCha/ChaProject/bbsApp/views.py
+++ ChaChaCha/ChaProject/bbsApp/views.py
@@ -1,6 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from .models import Academy_review, TestSite_review
 from django.http import HttpResponse
 from django.template import loader
 
+# 처음 리뷰 게시판 들어갔을 때, academy 정보 먼저 보여줌
+
 def review(request) :
-    return render(request, 'review.html', None)
+    page = request.GET.get('page', 1)
+
+    academyReview = Academy_review.objects.all()
+    reviewPaginator = Paginator(academyReview, 5)
+    reviewlistpage = reviewPaginator.get_page(page)
+
+    context = {"academyReview": reviewlistpage}
+    return render(request, 'review.html', context)
+
+def review_academy(request):
+    page = request.GET.get('page', 1)
+
+    academyReview = Academy_review.objects.all()
+    reviewPaginator = Paginator(academyReview, 5)
+    reviewlistpage = reviewPaginator.get_page(page)
+
+    context = {"academyReview": reviewlistpage}
+    return render(request, 'review_academy.html', context)
+
+
+def review_testsite(request):
+    page = request.GET.get('page', 1)
+
+    testsiteReview = TestSite_review.objects.all()
+    reviewPaginator = Paginator(testsiteReview, 5)
+    reviewlistpage = reviewPaginator.get_page(page)
+
+    context = {"testsiteReview": reviewlistpage}
+    return render(request, 'review_testsite.html', context)
+
+
+# 학원 리뷰 삭제
+def delete_academy(request):
+    pk = request.GET['pk']
+
+    delete_object = Academy_review.objects.get(pk=pk)
+    delete_object.delete()
+    return redirect("review_academy")
+
+# 시험장 리뷰 삭제
+def delete_testsite(request):
+    pk = request.GET['pk']
+
+    delete_object = TestSite_review.objects.get(pk=pk)
+    delete_object.delete()
+    return redirect("review_testsite")
