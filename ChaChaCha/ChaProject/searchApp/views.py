03c5da05c3b21853f5c48f635498897bb9e89b92
Soyoung Jang <76713906+so02e@users.noreply.github.com>
2021-02-09 09:37:04 +0900

    9시 36분

--- ChaChaCha/ChaProject/searchApp/views.py
+++ ChaChaCha/ChaProject/searchApp/views.py
@@ -39,9 +39,14 @@ def services1_search(request,city,town,name): #학원내용 출력
     context = {"sidos": sidos, 'method': 'get', 'dosi': dosi, 'gugun': town,'academy':name}
     return render(request, 'services1_search.html', context)
 
+
 # 내 주변 학원 검색 (맵)
 def services2(request) :
-    return render(request, 'services2.html', None)
+    mylocation = Academy.objects.all()
+    context = {
+        'Academy':mylocation,
+    }
+    return render(request, 'services2.html', context)
 
 # 내 주변 시험장 검색 (맵)
 def services3(request) :
@@ -49,11 +54,14 @@ def services3(request) :
     context = {
         'TestSite':mylocation,
     }
-
     return render(request, 'services3.html', context)
 
     ## thisUserMoments = mymoment.objects.filter(user=randomuserid, score__gte=4).order_by('-momentcreatedate', '-score')[0:4]
 # 지도 테스트 나중에 지우기
 def tt(request):
-    return render(request, 'tt.html', None)
+    mylocation = Academy.objects.all()
+    context = {
+        'Academy': mylocation,
+    }
+    return render(request, 'tt.html', context)
 
