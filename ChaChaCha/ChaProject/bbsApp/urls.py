550c9d05851dba5584d3199969811f126c6603c4
he-reme <rlagpfla1017@naver.com>
2021-02-15 15:37:06 +0900

    비밀번호 확인 후 삭제하기 추가

--- ChaChaCha/ChaProject/bbsApp/urls.py
+++ ChaChaCha/ChaProject/bbsApp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('review/', views.review_academy, name='review'),
     path('review_academy/', views.review_academy, name='review_academy'),
     path('review_testsite/', views.review_testsite, name='review_testsite'),
 
@@ -22,4 +21,7 @@ urlpatterns=[
     path('get_academy_town/', views.get_academy_town, name='get_academy_town'),
     path('get_academy_name/', views.get_academy_name, name='get_academy_name'),
     path('get_testsite_name/', views.get_testsite_name, name='get_testsite_name'),
+
+    path('get_academy_password/', views.get_academy_password, name='get_academy_password'),
+    path('get_testsite_password/', views.get_testsite_password, name='get_testsite_password'),
 ]
